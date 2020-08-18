4a46133a7fc6c6c95b86667203ab46ada1f78be9
Shubham Bhongale <shubhambhongale1@gmail.com>
2020-08-18 14:34:00 +0530

    corstay

--- app/src/main/java/com/example/corstay/Aswad.java
+++ app/src/main/java/com/example/corstay/Aswad.java
@@ -1,4 +1,14 @@
 package com.example.corstay;
 
-public class Aswad {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Aswad extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aswad);
+    }
 }
+
---
+++ app/src/main/java/com/example/corstay/Register.java
@@ -0,0 +1,6 @@
+package com.example.corstay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+}
