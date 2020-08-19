c5e008ddddbe183c98b85d81f4becf399b563353
Shubham Bhongale <shubhambhongale1@gmail.com>
2020-08-19 20:30:12 +0530

    done with the firebase authentication

--- app/src/main/java/com/example/corstay/MainActivity.java
+++ app/src/main/java/com/example/corstay/MainActivity.java
@@ -2,37 +2,87 @@ package com.example.corstay;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     public EditText user,password;
-    public Button loginButton;
+    public Button loginButton,signUp;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
         user=findViewById(R.id.USER);
         password=findViewById(R.id.PASSWORD);
         loginButton=findViewById(R.id.LOGIN);
+        signUp=findViewById(R.id.signup);
 
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+               /* if(FirebaseAuth.getInstance()!=null){
+                    startActivity(new Intent(getApplicationContext(),Hotel.class));
+                }*/
+            }
+        };
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                log();
+                signIn();
             }
 
         });
     }
 
-    public void log(){
-        Intent intent = new Intent(this, Hotel.class);
-        startActivity(intent);
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
+    public void signIn(){
+        String email=user.getText().toString();
+        String pass=password.getText().toString();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)){
+            Toast.makeText(MainActivity.this,"Fields are empty",Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+            mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Enter the valid UserId/Password",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        startActivity(new Intent(getApplicationContext(),Hotel.class));
+                    }
+                }
+            });
+        }
+
+    }
 }
--- app/src/main/java/com/example/corstay/Register.java
+++ app/src/main/java/com/example/corstay/Register.java
@@ -1,23 +1,61 @@
 package com.example.corstay;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.rengwuxian.materialedittext.MaterialEditText;
+
 
 public class Register extends AppCompatActivity {
     protected EditText Email,password,cpassword;
     protected Button singUp;
+    protected FirebaseAuth firebaseAuth;
     @Override
-    protected void onCreate (Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         Email=(MaterialEditText)findViewById(R.id.Remail);
         password=(MaterialEditText)findViewById(R.id.Rpassword);
         cpassword=(MaterialEditText)findViewById(R.id.Cpassword);
+        singUp=findViewById(R.id.Confirm);
+        firebaseAuth=FirebaseAuth.getInstance();
 
+        singUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uname=Email.getText().toString();            //user name
+                String pass=password.getText().toString();          //password
+                String cpass=cpassword.getText().toString();        //confirmed password
+
+                if(pass.equals(cpass)) {
+                    firebaseAuth.createUserWithEmailAndPassword(uname, pass)
+                            .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                        Toast.makeText(Register.this, "Registration complete", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                    else {
+                                        Toast.makeText(Register.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+
+        });
     }
 }
