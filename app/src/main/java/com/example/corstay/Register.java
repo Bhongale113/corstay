dc6a8c0bffbe2d95de12a2857942ef353f165809
Shubham Bhongale <shubhambhongale1@gmail.com>
2020-08-19 17:42:24 +0530

    after the Register

--- app/src/main/java/com/example/corstay/Register.java
+++ app/src/main/java/com/example/corstay/Register.java
@@ -1,12 +1,23 @@
 package com.example.corstay;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.rengwuxian.materialedittext.MaterialEditText;
+
 public class Register extends AppCompatActivity {
+    protected EditText Email,password,cpassword;
+    protected Button singUp;
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Email=(MaterialEditText)findViewById(R.id.Remail);
+        password=(MaterialEditText)findViewById(R.id.Rpassword);
+        cpassword=(MaterialEditText)findViewById(R.id.Cpassword);
+
     }
 }
